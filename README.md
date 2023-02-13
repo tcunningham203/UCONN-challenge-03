5c03abb0697cfde00cfda389892e665d74f459af
tcunningham203 <tcunningham203@gmail.com>
2023-02-14 04:42:29 +0900

    Prompt Routes

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 The third challenge for the Uconn bootcamp. A web application that generates a password when the button is pressed. 
 
 
+Screenshot link
+
+.\assets\css\style.css
+
+![AppScreenshot](/assets/screenshots/WEBSITE-SCREENSHOT.png?raw=true "Screenshot of Deployed Application")
+
+![PlanningPic](/assets/screenshots/routes-map.jpg?raw=true "Prompt Routes Picture")
+
+
 Commit Changes
 
 Commit 01: Initial Commit
@@ -15,3 +24,7 @@ Added files from UCONN, structured them, linked css to html
 Commit 03: Code Working
 
 Added Javascript to make the password function work. Has only 2 prompts so far.
+
+Commit 04: Prompt Routes
+
+Added all possible routes needed to generate the correct outcomes. Tested screenshot link in readme to see if it works.
--- script.js
+++ script.js
@@ -1,50 +1,174 @@
 //I learned how to create and retrieve data from a Prompt Dialog Box from this website http://www.learningaboutelectronics.com/Articles/How-to-create-and-retrieve-data-from-a-prompt-dialog-box-with-Javascript.php
 
-//I learned how to make randomly generated passwords from this website using Math.random() : https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+//I learned how to make randomly generated passwords from this website using Math.random() : https://dev.to/code_mystery/random-password-generator-using-javascript-6a However, it didnt quite work with this assignment so I tweaked things until it worked correctly.
 
-var charsLower = "qwertyuiopasdfghjklzxcvbnm"
-var charsUpper = "QWERTYUIOPASDFGHJKLZXCVBNM"
-var charsNumeric = "0123456789"
-var charsSpecial = "\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
-var charsLowerUpper = ""
-var charsLowerNumeric = ""
-var charsLowerSpecial = ""
-var charsUpperNumeric = ""
-var charsUpperSpecial = ""
-var charsNumericSpecial = ""
-var charsLowerUpperNumeric = ""
-var charsLowerUpperSpecial = ""
-var charsLowerNumericSpecial = ""
-var charsUpperNumericSpecial = ""
-var charsLowerUpperNumericSpecial = ""
+var charsL = "qwertyuiopasdfghjklzxcvbnm"
+var charsU = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var charsN = "0123456789"
+var charsS = "\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
+var charsLU = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+var charsLN = "qwertyuiopasdfghjklzxcvbnm0123456789"
+var charsLS = "qwertyuiopasdfghjklzxcvbnm\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
+var charsUN = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789"
+var charsUS = "QWERTYUIOPASDFGHJKLZXCVBNM\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
+var charsNS = "0123456789\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
+var charsLUN =  "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789"
+var charsLUS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
+var charsLNS = "qwertyuiopasdfghjklzxcvbnm0123456789\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
+var charsUNS = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
+var charsLUNS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
 
 
 function allPromptQuestions() {
 var passwordLength = prompt("How many characters would you like your password to be? Please input a number of at least 8 but no more than 128.")
 if (passwordLength >= 8 && passwordLength <= 128){
-  var promptLower = confirm("Would you like to have lowercase characters in your password? Press OK for yes. Press Cancel for no.");
+  var promptLower = confirm("Would you like to have lowercase characters in your password? \nPress OK for yes. Press Cancel for no.");
   if (promptLower == true) {
-  for (var i = 0; i <= passwordLength; i++) 
-  // var rng = Math.floor(Math.random() * charsLower.length);
-  password.value += charsLower[Math.floor(Math.random() * charsLower.length)]
-  console.log(password)
-  return password.value
+    var promptUpper = confirm("Would you like to have uppercase characters in your password? \nPress OK for yes. Press Cancel for no.");
+    if (promptUpper == true) {
+      var promptNumeric = confirm("Would you like to have numeric characters in your password? \nPress OK for yes. Press Cancel for no.");
+      if (promptNumeric == true) {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsLUNS[Math.floor(Math.random() * charsLUNS.length)]
+        return password.value
+        }
+        else {
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsLUN[Math.floor(Math.random() * charsLUN.length)]
+        return password.value
+        }
+        
+      }
+      else {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsLUS[Math.floor(Math.random() * charsLUS.length)]
+        return password.value
+        }
+        else {
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsLU[Math.floor(Math.random() * charsLU.length)]
+        return password.value
+        }
+        
+      }
+    }  
+    else {
+      var promptNumeric = confirm("Would you like to have numeric characters in your password? \nPress OK for yes. Press Cancel for no.");
+      if (promptNumeric == true) {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsLNS[Math.floor(Math.random() * charsLNS.length)]
+        return password.value
+        }
+        else {
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsLN[Math.floor(Math.random() * charsLN.length)]
+        return password.value
+        }
+        
+      }
+      else {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsLS[Math.floor(Math.random() * charsLS.length)]
+        return password.value
+        }
+        else {
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsL[Math.floor(Math.random() * charsL.length)]
+        return password.value
+        } 
+      }
+    } 
+  }  
+  else {
+    var promptUpper = confirm("Would you like to have uppercase characters in your password? \nPress OK for yes. Press Cancel for no.");
+    if (promptUpper == true) {
+      var promptNumeric = confirm("Would you like to have numeric characters in your password? \nPress OK for yes. Press Cancel for no.");
+      if (promptNumeric == true) {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsUNS[Math.floor(Math.random() * charsUNS.length)]
+        return password.value
+        }
+        else {
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsUN[Math.floor(Math.random() * charsUN.length)]
+        return password.value
+        }
+        
+      }
+      else {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsUS[Math.floor(Math.random() * charsUS.length)]
+        return password.value
+        }
+        else {
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsU[Math.floor(Math.random() * charsU.length)]
+        return password.value
+        }
+        
+      }
+    }  
+    else {
+      var promptNumeric = confirm("Would you like to have numeric characters in your password? \nPress OK for yes. Press Cancel for no.");
+      if (promptNumeric == true) {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsNS[Math.floor(Math.random() * charsNS.length)]
+        return password.value
+        }
+        else {
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsN[Math.floor(Math.random() * charsN.length)]
+        return password.value
+        }
+        
+      }
+      else {
+        var promptSpecial = confirm("Would you like to have special characters in your password? \nPress OK for yes. Press Cancel for no.");
+        if (promptSpecial == true){
+        for (var i = 0; i <= passwordLength; i++) 
+        password.value += charsS[Math.floor(Math.random() * charsS.length)]
+        return password.value
+        }
+        else{
+          alert("You must answer \"OK\" to at least one of the four prompts about character types to continue. Please start again.")
+          return ("Try again. Choose at least 1 character type.")
+         }
+      }
+    }   
   }
-   else{
-    alert("You must answer Y to at least one of the four prompts about character types to continue. Please start again.")
-    return ("Refresh and try again...")
-   }
-}else {
+}
+else {
 alert("You must input a number of at least 8 but no more than 128 to continue.")
-return ("Refresh and try again...")
+return ("Try again. Input a number within the specified range.")
+}
 }
 
+// Route 1-15 Code
+// for (var i = 0; i <= passwordLength; i++) 
+// password.value += charsLower[Math.floor(Math.random() * charsLower.length)]
+// return password.value
+// }
 
-}
 
-
-
-
+// Route 16 code
+// else{
+//   alert("You must answer Y to at least one of the four prompts about character types to continue. Please start again.")
+//   return ("Refresh and try again...")
+//  }
 
 
 
@@ -66,19 +190,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// var answer = window.prompt("Enter ");
-// if (answer) {
-//     //some code
-// }
-// else {
-//     //some code
-// }
-
-// document.querySelector('#generate').addEventListener('click' userAdjective); {
-// }
-// {
-//   function userAdjective = prompt("Please provide an Adjective");
-//   alert (userAdjective);
-// }
 
